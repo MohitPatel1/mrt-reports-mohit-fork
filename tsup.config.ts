0c5270d92424c631fe32ca8210a7aec74d0d732e
smarty <mohit.patel1966@gmail.com>
2025-04-25 11:48:00 +0530

    storybook biome added, eslint removed, tsup implemented

---
+++ tsup.config.ts
@@ -0,0 +1,17 @@
+import { type Options, defineConfig } from "tsup";
+const commons: Options = {
+  minify: false,
+  sourcemap: true,
+  dts: true,
+  clean: true,
+  target: "esnext",
+  external: ["react"],
+  format: ["esm", "cjs"],
+};
+export default defineConfig([
+  {
+    ...commons,
+    entryPoints: ["src/index.ts"],
+    outDir: "dist",
+  },
+]);
